2d4865506f4b747f4c10f85ca1169bcdfd59c795
lazwolv <lazlearnscode@gmail.com>
2024-02-20 17:36:52 -0800

    Java and HTML/CSS HW

--- CIT 130/Submitted Assignments/MartinezLazaroCit130Hw5.java
+++ CIT 130/Submitted Assignments/MartinezLazaroCit130Hw5.java
@@ -36,82 +36,104 @@ import java.util.Scanner;
 public class MartinezLazaroCit130Hw5{
     public static void main(String[] args) {
         
-        int speed;
-        int time;
-        int distance;
+        double speed, time, distance;
         int choice;
+        String name1, name2, name3, name4, temp;
+        boolean condition = true;
+        boolean order;
 
         Scanner input = new Scanner(System.in);
 
-        while (true) {
+        while (condition) {
             System.out.println("Select an option:");
             System.out.println("1. Compare names");
             System.out.println("2. Calculate car speed");
             System.out.println("3. Handling invalid input");
 
             choice = input.nextInt();
+            input.nextLine();
 
-            while (choice == 1 || choice == 2 || choice == 3){
-                if (choice == 1){
-                    System.out.println("Enter a name:");
-                    String name1 = input.nextLine();
-                    System.out.println("Enter a name:");
-                    String name2 = input.nextLine();
-                    System.out.println("Enter a name:");
-                    String name3 = input.nextLine();
-                    System.out.println("Enter a name:");
-                    String name4 = input.nextLine();
+            while (choice < 1 || choice > 3) {
+                System.out.println("You entered an out of range number.");
+                System.out.println("Select an option:");
+                choice = input.nextInt();
+                input.nextLine();
+            }
 
-                    if (name1.compareTo(name2) > 0){
-                        String temp = name1;
+
+            if (choice == 1) {
+                System.out.println("Enter the first name:");
+                name1 = input.nextLine();
+                System.out.println("Enter the second name:");
+                name2 = input.nextLine();
+                System.out.println("Enter the third name:");
+                name3 = input.nextLine();
+                System.out.println("Enter the fourth name:");
+                name4 = input.nextLine();
+
+                do {
+                    order = false;
+
+                    if (name1.compareTo(name2) > 0) {
+                        temp = name1;
                         name1 = name2;
                         name2 = temp;
+                        order = true;
                     }
-                    if (name2.compareTo(name3) > 0){
-                        String temp = name2;
+                    if (name2.compareTo(name3) > 0) {
+                        temp = name2;
                         name2 = name3;
                         name3 = temp;
+                        order = true;
                     }
-                    if (name3.compareTo(name4) > 0){
-                        String temp = name3;
+                    if (name3.compareTo(name4) > 0) {
+                        temp = name3;
                         name3 = name4;
                         name4 = temp;
+                        order = true;
                     }
-                    if (name1.compareTo(name2) > 0){
-                        String temp = name1;
+                    if (name1.compareTo(name2) > 0) {
+                        temp = name1;
                         name1 = name2;
                         name2 = temp;
+                        order = true;
+                    }
+                } while (order);
+
+                System.out.printf("In alphabetical order:\n%s\t%s\t%s\t%s\n", name1, name2, name3, name4);
+
+                for (int i = 2; i < name1.length(); i+=2) {
+                        System.out.print(name1.charAt(i));
                     }
 
-                    
-
-                } else if (choice == 2){
-                    System.out.println("Enter the speed:");
-                    speed = input.nextInt();
-                    System.out.println("Enter the time:");
-                    time = input.nextInt();
-                        while (time < 0){
-                            System.out.println("Enter a valid time value:");
-                            int time = input.nextInt();
-                        }
-                    int distance = speed * time;
-                    System.out.printf("Time\tDistance\tSpeed\n");
-                    System.out.printf("%d\t %d\t %d\n", time, distance, speed);
-                    
-                    while (speed > 0 && speed < 120){
-                        speed = speed + 5;
-                        time = time + 10;
-                        distance = speed * time;
-                        System.out.printf("%d\t%d\t%d\n", time, distance, speed);
+            } else if (choice == 2){
+                System.out.println("Enter the speed:");
+                speed = input.nextInt();
+                System.out.println("Enter the time:");
+                time = input.nextInt();
+                    while (time < 0){
+                        System.out.println("Enter a valid time value:");
+                        time = input.nextInt();
                     }
-                else {
+                distance = speed * time;
+                System.out.printf("Hour\tSpeed\tDistance\n");
+                System.out.printf("%.2f\t%.2f\t%.2f\n", time, speed, distance);
+                
+                while (speed > 0 && speed < 120){
+                    speed = speed + 5;
+                    time = time + 10;
+                    distance = speed * time;
+                    System.out.printf("%.2f\t%.2f\t%.2f\n", time, speed, distance);
+                }
+            } else if (choice == 3){
+                System.out.println("You entered an out of range number.");
 
                 }
+        System.out.println("Would you like another selection? True or False?");
+        condition = input.nextBoolean();
+    
         }
-    
-    
-    
-    
+
     }
 
 }
